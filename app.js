b80fb430ab181ddc754e6f6e442c0923cf8da6be
Preetesh21 <verma.preetesh21@gmail.com>
2021-05-04 10:15:30 +0530

    done

--- app.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-app.listen(3000, () => { console.log("Listening on port 3000") })
-app.use(express.static('public'))
-app.use(express.json({ limit: '2mb' }));
